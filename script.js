c517462590ecf2353aca01adcc50aab1dd80bce1
Dikesh Khagi <dikeshkhagi1@gmail.com>
2024-09-12 11:48:49 +0545

    add keywords in meta tag, added some quotes

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ const quotes ={
         "Science is a way of thinking much more than it is a body of knowledge. – Carl Sagan",
         "Science knows no country, because knowledge belongs to humanity, and is the torch which illuminates the world. – Louis Pasteur",
         "Somewhere, something incredible is waiting to be known.” – Carl Sagan",
-
-        
     ],
     management:[
         "The art of leadership is saying no, not yes. It is very easy to say yes – Tony Blair.",
         "Management is doing things right; leadership is doing the right things – Peter Drucker.",
         "People want to know they matter and they want to be treated as people. That’s the new talent contract – Pamela Stroko.",
-        "Time management is an oxymoron. Time is beyond our control, and the clock keeps ticking regardless of how we lead our lives. Priority management is the answer to maximizing the time we have – John C. Maxwell."
+        "Time management is an oxymoron. Time is beyond our control, and the clock keeps ticking regardless of how we lead our lives. Priority management is the answer to maximizing the time we have – John C. Maxwell.",
+        "The way to get started is to quit talking and begin doing. – Walt Disney"
     ],
     sports:[
         "Champions keep playing until they get it right  – Billie Jean King.",
         "The only way to prove that you’re a good sport is to lose – Ernie Banks." ,
         "It's not whether you get knocked down; it's whether you get up – Vince Lombardi.",
         "The more difficult the victory, the greater the happiness in winning – Pelé.",
-        "Hard work beats talent when talent doesn’t work hard – Tim Notke." 
+        "Hard work beats talent when talent doesn’t work hard – Tim Notke.",
+        "Success is where preparation and opportunity meet. – Bobby Unser"
     ]
 };
 let currentCategory = 'science';
